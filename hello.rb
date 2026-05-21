1d3541a24e8b13a804cd8c8b18a8bb0cfb8ed3b1
Manish10022001 <manishshirsat2850@gmail.com>
2026-05-21 21:16:58 +0530

    first ruby program -Hello world(puts vs print)

---
+++ hello.rb
@@ -0,0 +1,9 @@
+# puts string on new line
+puts "Hello World";
+puts "My name is Manish";
+
+#prints -> print line on the same line
+print "Hello World";
+puts " my name is Ajinkya"
+
+#------------------------------------------------------------------
