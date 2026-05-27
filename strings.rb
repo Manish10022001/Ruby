448a7350def121e25bb66c6a143aeb5992f2189d
Manish10022001 <manishshirsat2850@gmail.com>
2026-05-27 22:49:18 +0530

    Strings

--- strings.rb
+++ strings.rb
@@ -17,3 +17,29 @@ puts "String Interpolation"
 name = "Manish"
 puts "Hello, #{name}" #"Hello, Manish"
 puts 'Hello, #{name}' #"Hello, #{name}"
+
+#Strings methods
+puts "\n\nSTRING METHODS" 
+
+# capitalize : to capitalize first char of string
+puts "hello".capitalize     #Hello
+
+# include? : to see if chars are there in the string returns true or false
+puts "hello".include?("lo") # true
+puts "hello".include?("z")  #false
+
+# upcase : to make whole string capital letters
+puts "hello".upcase         #"HELLO"
+
+# downcase : to make whole string lowercase
+puts "HELLO".downcase       #"hello"
+
+# empty? : to check if string is empty or not -> return true or false
+puts "hello".empty?         #false as string is not empty
+puts "".empty?              #true as string is empty
+
+# length : to get the length of string
+puts "hello".length         #5
+
+# reverse : to reverse a string
+puts "hello".reverse
